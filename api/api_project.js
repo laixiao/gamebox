79124eb3b328c54bb2891654d4d7a8a267599a9b
laixiao <2515097216@qq.com>
2019-01-28 17:39:59 +0800

    sdk新增道具和金币接口

--- api/api_project.js
+++ api/api_project.js
@@ -9,7 +9,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-12-21T04:58:55.028Z",
+    "time": "2019-01-28T09:39:26.631Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
